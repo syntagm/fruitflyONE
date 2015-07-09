9eef71ff4e70ad55a29ccdd38ff1a919014f6ecb
syntagm <syntagm@icon.co.za>
2015-07-09 15:47:22 +0200

    appLoad 0907_01
    moved the input form to about. appjs changed.

--- www/js/app.js
+++ www/js/app.js
@@ -64,7 +64,50 @@ var example = angular.module('ionicApp', ['ionic'])
    $urlRouterProvider.otherwise("/tab/home");
 
 })
-
+// the controller determines what will be shown in the view the html part
+// it is called Home Tab Controller
+/* the controller and view share an object called a scope;
+this object is at the core of its amazing two-way data binding.
+The controller sets properties on the scope, and the view binds to those properties.
+*/
 .controller('HomeTabCtrl', function($scope) {
   console.log('HomeTabCtrl');
 });
+
+
+
+Parse.initialize("VeOAN2nRQDXf2CJ1aypkAeVGIhHbSuI05b1Hwlgd", "BhlQJ6JTZ9mV0kBhViVOKeFPddNKubrz5camfsS8");
+
+example.controller("ExampleController", function($scope) {
+
+var Post = Parse.Object.extend("Post");
+$("#post-form").submit(function(event){
+  event.preventDefault();
+    var title = $("#post-title").val();
+    var content = $("#post-content").val();
+    var geoPoint = $("#post-geoPoint").val();
+
+    var newPost = new Post();
+    newPost.set("title", title);
+    newPost.set("content", content);
+    newPost.set("geoPoint", geoPoint);
+
+    newPost.save({
+      success: function(){
+
+      }, error: function(error){
+          console.log("Error:" *error.message);
+      }
+    });
+});
+
+});
+
+/*
+Parse.GeoPoint.current({
+    success: function (point) {
+        //use current location
+        console.log(point);
+    }
+});
+*/
